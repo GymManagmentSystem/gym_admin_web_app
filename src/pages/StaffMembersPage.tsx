6c909ca7c60010cf383195a8c4684f14ac721d8d
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-04-04 10:38:38 +0530

    adding Text When Empty Staff Member List

--- src/pages/StaffMembersPage.tsx
+++ src/pages/StaffMembersPage.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import SearchHeadingBar from "../components/SearchHeadingBar";
 import TableComponent from "../components/TableComponent";
 import { useNavigate } from "react-router-dom";
@@ -49,14 +49,25 @@ const StaffMembersPage = () => {
           onSearch={(term) => setSearchTerm(term)}
           buttonPressed={buttonPress}
           heading="Staff Members"
-          buttonText="Add Member"
+          buttonText="Add Staff"
         />
-        <TableComponent
-          thArray={["MemberId", "Name", "Phone", "Gender", "Action"]}
-          onPressViewButton={viewButtonPressed}
-          personArray={filterMembersArray ? filterMembersArray : []}
-          arrayType="Staff"
-        />
+        {filterMembersArray && filterMembersArray.length > 0 ? (
+          <TableComponent
+            thArray={["MemberId", "Name", "Phone", "Gender", "Action"]}
+            onPressViewButton={viewButtonPressed}
+            personArray={filterMembersArray ? filterMembersArray : []}
+            arrayType="Staff"
+          />
+        ) : (
+          <Box textAlign="center" mt={10} color="gray.600">
+            <Heading size="lg">No Staff Members Yet!</Heading>
+
+            <Text fontSize="xl" mt={3}>
+              Build your team to manage your gym efficiently!Tap Add Staff to
+              recruit your first team member.
+            </Text>
+          </Box>
+        )}
       </Box>
     </>
   );
